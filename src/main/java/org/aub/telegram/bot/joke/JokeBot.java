5f7e545115760cd5ff4835447bb21712a192de4c
Sergii Litvinov <slitvinov_ext@icontrol.com>
2016-06-10 16:57:12 +0300

    Add stats

--- src/main/java/org/aub/telegram/bot/joke/JokeBot.java
+++ src/main/java/org/aub/telegram/bot/joke/JokeBot.java
@@ -1,5 +1,8 @@
 package org.aub.telegram.bot.joke;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.botan.sdk.Botan;
+import jersey.repackaged.com.google.common.collect.ImmutableMap;
 import org.telegram.telegrambots.TelegramApiException;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
@@ -7,6 +10,8 @@ import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.logging.BotLogger;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +19,16 @@ import java.util.List;
 public class JokeBot extends TelegramLongPollingBot {
     private static final String TAG = "JokeBot";
     private JokeDb jokeDb = new JokeDb();
+    private Botan botan;
+
+    public JokeBot() {
+        try (CloseableHttpAsyncClient client = HttpAsyncClients.createDefault()) {
+            client.start();
+            botan = new Botan(client, new ObjectMapper());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void onUpdateReceived(Update update) {
@@ -24,7 +39,8 @@ public class JokeBot extends TelegramLongPollingBot {
         sendMessage.setChatId(String.valueOf(update.getMessage().getFrom().getId()));
         try {
             sendMessage(sendMessage);
-        } catch (TelegramApiException e) {
+            botan.track("pE5rfY-CM0rQBsN9_7wS8jYG42a:1yml", update.getMessage().getFrom().getId().toString(), ImmutableMap.of("some_metric:", 100, "another_metric", 500), update.getMessage().getText()).get();
+        } catch (Exception e) {
             BotLogger.error(TAG, e.getMessage());
         }
     }
